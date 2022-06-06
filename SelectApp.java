c45af5ae99f6d660a59674198e679696dd5f360f
kavandotcom <67421454+kavandotcom@users.noreply.github.com>
2022-06-06 08:26:50 +0530

    Create SelectApp.java

---
+++ SelectApp.java
@@ -0,0 +1,65 @@
+package net.sqlitetutorial;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class SelectApp {
+
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(){
+        String sql = "SELECT movie, actor, actress, year, director FROM movies";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(
+                                   rs.getString("movie") + "\t" +
+                                   rs.getString("actor") + "\t" +
+                                   rs.getString("actress") + "\t" +
+                                   rs.getInt("year") +  "\t" + 
+                                   rs.getString("director"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.selectAll();
+    }
+
+}
